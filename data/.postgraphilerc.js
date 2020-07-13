b3f252cb6cefca3eeba7411d4fb114643337e302
Héctor Menéndez <hector@gik.mx>
2020-07-13 14:45:23 -0500

    #8: Using only determined schemas on postgraphile.

--- data/.postgraphilerc.js
+++ data/.postgraphilerc.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
+const { DB_SCHEMA } = require("./constants");
 
 const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_DB } = process.env;
 
 module.exports = {
     options: {
         connection: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:${POSTGRES_PORT}/${POSTGRES_DB}`,
-        schema: ["public"],
+        schema: Object.values(DB_SCHEMA).filter(val => val !== DB_SCHEMA.PRIVATE),
         watch: true,
         dynamicJson: true,
-        subscriptions: true,
+        subscriptions: false,
         enhanceGraphiql: true,
     },
 };
