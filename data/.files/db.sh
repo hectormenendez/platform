4913414095c2b123f049da18774c3181ee60fc6c
Héctor Menéndez <hector@gik.mx>
2020-07-30 16:59:20 -0500

    #8: removed knex, will be using own solution.

--- data/.files/db.sh
+++ data/.files/db.sh
@@ -3,7 +3,6 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 
-    -- @see knexfile.js
-    CREATE SCHEMA IF NOT EXISTS knex;
+    -- Put here every statement you want to run on container creation
 
 EOSQL
--- data/src/index.js
+++
@@ -1,1 +0,0 @@
-console.log("hola"); // eslint-disable-line no-console
